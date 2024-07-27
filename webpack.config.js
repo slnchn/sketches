70f47ecd36efc9afa30d2947fe492a50a5fc1e8c
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-27 20:05:33 +0200

    add tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,58 @@
 const path = require("node:path");
+
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
-module.exports = {
-  entry: {
-    waves: "./src/pages/waves/waves.js",
-    "white-noise": "./src/pages/white-noise/white-noise.js",
-    index: "./src/pages/index.js",
-  },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === "production";
 
-  output: {
-    path: path.resolve(__dirname, "./dist"),
-    // filename: "[name]/[name].js",
-    clean: true,
-  },
+  return {
+    entry: {
+      waves: "./src/pages/waves/waves.js",
+      "white-noise": "./src/pages/white-noise/white-noise.js",
+      index: "./src/pages/index.js",
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "waves.html",
-      template: "./src/pages/waves/waves.html",
-      chunks: ["waves"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "white-noise.html",
-      template: "./src/pages/white-noise/white-noise.html",
-      chunks: ["white-noise"],
-    }),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/pages/index.html",
-      chunks: ["index"],
-    }),
-  ],
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+      clean: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        },
+      ],
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+      }),
+
+      new HtmlWebpackPlugin({
+        filename: "waves.html",
+        template: "./src/pages/waves/waves.html",
+        chunks: ["waves"],
+      }),
+      new HtmlWebpackPlugin({
+        filename: "white-noise.html",
+        template: "./src/pages/white-noise/white-noise.html",
+        chunks: ["white-noise"],
+      }),
+      new HtmlWebpackPlugin({
+        filename: "index.html",
+        template: "./src/pages/index.html",
+        chunks: ["index"],
+      }),
+    ],
+
+    optimization: {
+      minimize: isProduction,
+      minimizer: [`...`, new CssMinimizerPlugin()],
+    },
+  };
 };
