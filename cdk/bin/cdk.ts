ed26630de3e6ffc8b21daca02337df8d2b4a1f93
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-07-28 16:43:21 +0200

    tempo

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
+
 import { CdkStack } from "../lib/cdk-stack";
 
 const app = new cdk.App();
-new CdkStack(app, "CdkStack", {
+new CdkStack(app, "slnchn-sketches-stack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
