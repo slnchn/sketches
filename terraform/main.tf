f318134c8b09fe8d2ffcf2d4c83af5aecb6a799c
Aliaksandr Kavalenka <aliaksandr.kavalenka0236@gmail.com>
2024-10-13 22:26:54 +0200

    fix s3 and cloudfront spa routing

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,16 +20,6 @@ resource "aws_s3_bucket_website_configuration" "slnchn_sketches_bucket" {
   error_document {
     key = "index.html"
   }
-
-  routing_rule {
-    condition {
-      http_error_code_returned_equals = "404"
-    }
-
-    redirect {
-      replace_key_with = "index.html"
-    }
-  }
 }
 
 resource "aws_s3_bucket_public_access_block" "slnchn_sketches_bucket" {
@@ -95,10 +85,17 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   }
 
   custom_error_response {
-    error_caching_min_ttl = 300
+    error_code            = 403
+    response_page_path     = "/index.html"
+    response_code          = 200
+    error_caching_min_ttl  = 0
+  }
+
+  custom_error_response {
     error_code            = 404
-    response_code         = 200
-    response_page_path    = "/index.html"
+    response_page_path     = "/index.html"
+    response_code          = 200
+    error_caching_min_ttl  = 0
   }
 
   price_class = "PriceClass_100"
